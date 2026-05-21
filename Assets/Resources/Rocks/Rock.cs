18ad351cf2c649afaade90c5123940438087728b
Kaybold <monika.carr.2004@gmail.com>
2026-05-21 17:44:36 +1200

    deserialization works again
    due to files being moved it broke, but i changed where it was looking, so it'd go for the right one :]

--- Assets/Resources/Rocks/Rock.cs
+++ Assets/Resources/Rocks/Rock.cs
@@ -6,19 +6,29 @@ using System.Runtime.CompilerServices;
 
 public class Rock : MonoBehaviour
 {
-    public string RockName { get; private set; }
-    public string RockType { get; private set; }
-    public string WorldRegion { get; private set; }
-    public string Composition { get; private set; }
-    public double Density { get; private set; }
-    public int MeltingPoint { get; private set; }
-    [SerializeField] public string fileName = "test";
+    public string RockName;
+    public string RockType;
+    public string WorldRegion;
+    public string Composition;
+    public double Density;
+    public int MeltingPoint;
+    public string fileName = "test";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        string filePathJson = "Assets/Rocks/Data/" + fileName + ".json";
-        string filePathMesh = "Assets/Rocks/Models/" + fileName;
-        string filePathMaterial = "Assets/Rocks/Materials/" + fileName;
+        Refresh();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Refresh()
+    {
+
+        string filePathJson = "Assets/Resources/Rocks/Data/" + fileName + ".json";
+        string filePathMesh = "Rocks/Models/" + fileName;
+        string filePathMaterial = "Rocks/Materials/" + fileName;
         string[] fileText = File.ReadAllLines(filePathJson);
         string fileTextOneLine = "";
         for (int i = 0; i < fileText.Length; i++)
@@ -32,15 +42,10 @@ public class Rock : MonoBehaviour
         this.Composition = temp.Composition;
         this.Density = temp.Density;
         this.MeltingPoint = temp.MeltingPoint;
-        // Debug.Log(RockName + ", " + RockType + ", " + WorldRegion + ", " + Composition + ", " + Density + ", " + MeltingPoint);
+        Debug.Log(RockName + ", " + RockType + ", " + WorldRegion + ", " + Composition + ", " + Density + ", " + MeltingPoint);
         Material materialToChange = this.gameObject.transform.Find("Render").GetComponentInChildren<MeshRenderer>().material;
         MeshFilter meshToChange = this.gameObject.transform.Find("Render").GetComponentInChildren<MeshFilter>();
         materialToChange = (Material)Resources.Load(filePathMaterial);
         meshToChange.sharedMesh = (Mesh)Resources.Load(filePathMesh);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
