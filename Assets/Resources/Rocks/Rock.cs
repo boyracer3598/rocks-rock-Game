7f92d59a5aef1f22b8a9311239a49e413c2cb0b6
Kaybold <monika.carr.2004@gmail.com>
2026-05-26 16:03:03 +1200

    rock redesign

--- Assets/Resources/Rocks/Rock.cs
+++ Assets/Resources/Rocks/Rock.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
 
+public class SubRock
+{
+    public string RockName;
+    public string RockType;
+    public string RockVariety;
+    public string Composition;
+    public double Density;
+    public int MeltingPoint;
+}
 public class Rock : MonoBehaviour
 {
     public string RockName;
     public string RockType;
-    public string WorldRegion;
+    public string RockVariety;
     public string Composition;
     public double Density;
     public int MeltingPoint;
@@ -35,14 +44,14 @@ public class Rock : MonoBehaviour
         {
             fileTextOneLine += fileText[i];
         }
-        Rock temp = JsonConvert.DeserializeObject<Rock>(fileTextOneLine);
+        SubRock temp = JsonConvert.DeserializeObject<SubRock>(fileTextOneLine);
         this.RockName = temp.RockName;
         this.RockType = temp.RockType;
-        this.WorldRegion = temp.WorldRegion;
+        this.RockVariety = temp.RockVariety;
         this.Composition = temp.Composition;
         this.Density = temp.Density;
         this.MeltingPoint = temp.MeltingPoint;
-        Debug.Log(RockName + ", " + RockType + ", " + WorldRegion + ", " + Composition + ", " + Density + ", " + MeltingPoint);
+        Debug.Log(RockName + ", " + RockType + ", " + RockVariety + ", " + Composition + ", " + Density + ", " + MeltingPoint);
         Material newMaterial = (Material)Resources.Load(filePathMaterial);
         GetComponent<Renderer>().material = newMaterial;
         GameObject model = Resources.Load<GameObject>(filePathMesh);
