3acf4fabd1a95bbd2038e50ed64b409d331a8b2c
Kaybold <monika.carr.2004@gmail.com>
2026-05-29 17:52:52 +1200

    UNFINISHED SCALE MACHINE
    It's scarifying

--- Assets/Resources/Objects/Scale/ScaleLogic.cs
+++ Assets/Resources/Objects/Scale/ScaleLogic.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ScaleLogic : MonoBehaviour
 {
     public bool leftHeavy = true;
+    public GameObject left, right;
     double calculateDifference(double a, double b) //a = left b = right
     {
         double result;
@@ -20,22 +21,33 @@ public class ScaleLogic : MonoBehaviour
         }
         return result;
     }
-    void MachineStart(GameObject a, GameObject b)
+    void MachineStart()
     {
-        if (a.gameObject.GetComponentInChildren<Rock>().RockName != "" && b.gameObject.GetComponentInChildren<Rock>().RockName != "")
+        if (left.GetComponent<Rock>().RockName != "" && right.GetComponent<Rock>().RockName != "")
         {
-            
+            int heavyToInt =0;
+            double ghetsis = calculateDifference(left.GetComponent<Rock>().Density, right.GetComponent<Rock>().Density);
+            Debug.Log(ghetsis);
+            if (leftHeavy == true)
+            {
+                heavyToInt = 1;
+            }
+            else
+            {
+                heavyToInt = -1;
+            }
+            GameObject.FindWithTag("Bowl1").transform.transform.position = new Vector3 (0, (float)(0 + (1000 / (ghetsis * heavyToInt))), 0);
         }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        MachineStart();
     }
 }
