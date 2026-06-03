05c1608419c27dcf7d9bd7a5afcd6715270025e5
Cole <poby19@gmail.com>
2026-06-03 21:24:43 +1200

    finished lighting and added glowworms and other features

---
+++ Assets/Resources/Cave/Bat/BatMove.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BatMove : MonoBehaviour
+{
+    [SerializeField] Transform[] Points;
+    [SerializeField] float Speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
