7ee82afe9e25c8497693113203f16eec1411e5f0
Kaybold <monika.carr.2004@gmail.com>
2026-06-09 17:07:27 +1200

    Dialogue Testing
    Created the json file, begun work on the quest types

--- Assets/Resources/NPC/QuestDialogue.cs
+++ Assets/Resources/NPC/QuestDialogue.cs
@@ -3,13 +3,17 @@ using Kay.Data;
 using NUnit.Framework;
 using System.IO;
 using Newtonsoft.Json;
+using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using System.Linq;
 public class Rocks
 {
     public string[] RockNameList;
 }
 public class QuestDialogue : MonoBehaviour
 {
-    KayStack<string> RockNames;
+    KayStack<string> RockNames = new();
+    Dictionary<string, string> QuestTypes = new();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,14 +25,22 @@ public class QuestDialogue : MonoBehaviour
             fileTextOneLine += fileText[i];
         }
         Rocks temp = JsonConvert.DeserializeObject<Rocks>(fileTextOneLine);
-        for (int i =0; i < temp.RockNameList.Length; i++)
+        for (int i = 0; i < temp.RockNameList.Length; i++)
         {
-            RockNames.Append(temp.RockNameList[i]);
+            this.RockNames.Append(temp.RockNameList[i]);
         }
+        QuestTypes.Add("Find", "Bring me a");
+        QuestTypes.Add("HeavierThan", "Bring me a rock that's heavier than");
+        QuestTypes.Add("LighterThan", "Bring me a rock that's lighter than");
+        QuestTypes.Add("Combusts", "Bring me a rock that combusts");
+        QuestTypes.Add("Melts", "Bring me a rock that melts");
+        NewQuest();
     }
     void NewQuest()
     {
         string GimmeARock = RockNames.GrabRandom();
+        string RandomQuestType = QuestTypes.ElementAt(3).Value;
+        Debug.Log(RandomQuestType + GimmeARock);
     }
     // Update is called once per frame
     void Update()
